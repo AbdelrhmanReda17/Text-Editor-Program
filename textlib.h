e22b8f53e7d3a678d07369ae58bf79b7ee8c8dab
Abdelrhman R,Muhammed <abdelrhmanrmuhammed@gmail.com>
2022-05-14 21:26:54 +0200

    Last Edit

---
+++ textlib.h
@@ -0,0 +1,52 @@
+/*
+FCAI – Programming 1 – 2022 - Assignment 4
+Program Name: CS112-203-2nd–S1,2-20210255-20210376-20210436-A4-TextEditorApp.cpp
+Program Description: Text Editor Application
+Last Modification Date: 12 / 5 / 2022.
+Author1 and ID and Group: Abdelrhman Reda Mohamed- 20210255- GROUP A
+Author2 and ID and Group: Mahmoud Mamdouh Abdelaziz-20210376- GROUP A
+Author3 and ID and Group: Nourhan Elsayed Hassan Ibrahim-20210436- GROUP A
+Teaching Assistant: Eng. Nesma & Eng. Yousra
+Purpose: Applying a variety edits on desired text files
+ */
+
+
+#ifndef TEXTLIB_H_
+#define TEXTLIB_H_
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <fstream>
+#include <unistd.h>
+#include <cstring>
+#include <cctype>
+
+using namespace std;
+
+
+
+// Functions' Prototypes
+inline void upper(string& s);
+void temptxt(char []);
+int mainmessage(char []);
+void first_choice();
+void second_choice(char[]);
+void third_choice();
+void fourth_choice();
+void fifth_choice();
+void sixth_choice();
+void seventh_choice();
+void eighth_choice();
+void ninth_choice();
+void tenth_choice();
+void eleventh_choice();
+void twelfth_choice();
+void thirteenth_choice();
+void fourteenth_choice();
+void save(char []);
+
+
+
+#endif // TEXTLIB_H_
