1cc58cbb9980533167ed6c93db28854637b57e6d
NourhanE.Hassan <92875566+nouribrahem@users.noreply.github.com>
2022-05-16 19:21:26 +0200

    removed program header

--- textlib.cpp
+++ textlib.cpp
@@ -1,14 +1,3 @@
-/*
-FCAI – Programming 1 – 2022 - Assignment 4
-Program Name: CS112-203-2nd–S1,2-20210255-20210376-20210436-A4-TextEditorApp.cpp
-Program Description: Text Editor Application
-Last Modification Date: 12 / 5 / 2022.
-Author1 and ID and Group: Abdelrhman Reda Mohamed- 20210255- GROUP A
-Author2 and ID and Group: Mahmoud Mamdouh Abdelaziz-20210376- GROUP A
-Author3 and ID and Group: Nourhan Elsayed Hassan Ibrahim-20210436- GROUP A
-Teaching Assistant: Eng. Nesma & Eng. Yousra
-Purpose: Applying a variety edits on desired text files
- */
 
 #include "textlib.h"
 
